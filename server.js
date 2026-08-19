f03e79d68a116008434352901da583afc93044dd
Sebby <sebastianyalonso08@gmail.com>
2026-08-19 12:32:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,37 @@ function resolveUserKey(users, identifier) {
 
     return null;
 }
+const heartbeat = setInterval(() => {
+  for (const ws of wss.clients) {
+    if (ws.isAlive === false) {
+      ws.terminate();
+      continue;
+    }
+
+    ws.isAlive = false;
+    ws.ping();
+  }
+}, 25000);
+
+function shutdown() {
+  clearInterval(heartbeat);
+
+  for (const ws of wss.clients) {
+    try {
+      ws.close(1001, "Server restarting");
+    } catch {}
+  }
+
+  server.close(() => process.exit(0));
+  setTimeout(() => process.exit(0), 5000).unref();
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+
+ensureDataDir();
+
+server.listen(PORT, HOST, () => {
+  console.log("EpicBloxs GLOBAL server on http://" + HOST + ":" + PORT);
+  console.log("Users file: " + usersPath);
+});
